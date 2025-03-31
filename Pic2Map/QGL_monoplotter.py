f8a42c6e8887743b6c4e6e7cb35ab5445e7659de
Johary Rakotomalala <johary.rakotomalala@ig-group.ch>
2025-03-31 16:18:28 +0200

    Remove opengl import

--- Pic2Map/QGL_monoplotter.py
+++ Pic2Map/QGL_monoplotter.py
@@ -19,7 +19,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtOpenGL import *
-from OpenGL.GLU import *
+# from OpenGL.GLU import *
 from numpy import sqrt, cos, sin, array, pi, linalg, dot
 # from OpenGL.GL.ARB.vertex_buffer_object import *
 # from OpenGL.arrays import ArrayDatatype as ADT
